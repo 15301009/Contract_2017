3f1d1e2678a237b9a4d95879812f852432a5ec66
DumpsterCC <1790201551@qq.com>
2017-06-06 18:07:36 +0800

    main ui

--- frame1.php
+++ frame1.php
@@ -1,18 +1,17 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<!DOCTYPE HTML>
 <html>
 <head>
 <title>合同管理系统 - 管理员</title>
 <link href="css/frame.css" rel="stylesheet" type="text/css" />
-
-<?php header("Content-Type:text/html;charset=gbk"); ?>
+<meta http-equiv="Content-Type" content="text/html; charset=gbk" />
 </head>
 
 <!-- Frame structure:upper, middle (left, right), under-->
 <frameset rows="100,*,70" frameborder="no" border="0">
 	<frame src="header2.php" scrolling="no" />
 	<frameset cols="200,*">
-		<frame src="left1.htm" scrolling="no" />
-		<frame src="welcome.htm" name="main" scrolling="auto" />
+		<frame src="left1.html" scrolling="no" />
+		<frame src="welcome.html" name="main" scrolling="auto" />
 	</frameset>
 	<frame src="footer2.htm" scrolling="no" />
 </frameset>
